7d9d27c94f82f43aaf1e69ca0c752de2f268e910
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2020-11-06 22:23:00 +0100

    Added game function which plays 5 times a game.

--- gameLogic.js
+++ gameLogic.js
@@ -1,26 +1,49 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function computerPlay () {
     const possibleChoices = ["rock", "paper", "scissors"];
-    return Math.floor((Math.random() * possibleChoices.length));
+    return possibleChoices[Math.floor((Math.random() * possibleChoices.length))];
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection) {
         return "Its a tie!";
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        playerScore++;
         return "You win! Rock beats scissors."
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        playerScore++;
         return "You win! Paper beats rock!."
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return "You win! Scissors beats paper!."
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+        computerScore++;
         return "You lose! Paper beats rock!."
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return "You lose! Rock beats scissors!."
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return "You lose! Scissors beats paper!."
     } else {
         return "Something went wrong, is your input correct?"
     }
 }
 
-
+function game() {
+    for(let roundPlayed = 0; roundPlayed <= 5; roundPlayed++) {
+        const computerSelection = computerPlay();
+        let playerSelection = prompt("What do you choose? Rock, paper, scissors?")
+        alert(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore < computerScore) {
+        alert("Sorry, you lost this time.\nYour score: " + playerScore + "\nComputer score: "+ computerScore)
+    } else if (playerScore > computerScore) {
+        alert("Nice! You win.\nYour score: " + playerScore + "\nComputer score: "+ computerScore)
+    } else {
+        alert("Tied game.")
+    }
+}
